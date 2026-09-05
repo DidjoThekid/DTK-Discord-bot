8dc8a56deb59b7fa7cfc4ad1b05278dbc86846a0
Djordan <didjothekid@gmail.com>
2026-09-05 09:05:35 +0200

    Add LinkDiscordButton component

---
+++ src/components/LinkDiscordButton.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LinkDiscordButton() {
+  return (
+    <a href="/api/discord/oauth/start" className="btn-primary whitespace-nowrap">
+      Lier Discord
+    </a>
+  );
+}
