95da9edd2d0063daa317061866178f810d58ee85
Djordan <didjothekid@gmail.com>
2026-09-05 14:11:51 +0000

    Fix merge conflict create admin

--- prisma/create-admin.ts
+++ prisma/create-admin.ts
@@ -9,41 +9,28 @@ async function main() {
 
   const passwordHash = await bcrypt.hash(password, 10);
 
-<<<<<<< HEAD
   const admin = await prisma.user.upsert({
-=======
-  const user = await prisma.user.upsert({
->>>>>>> 7824227 (Fix login admin)
     where: {
-      email: email,
+      email,
     },
     update: {
       isAdmin: true,
       isVerified: true,
     },
     create: {
-      email: email,
-      passwordHash: passwordHash,
+      email,
+      passwordHash,
       isAdmin: true,
       isVerified: true,
     },
   });
 
-<<<<<<< HEAD
   console.log("Compte admin créé :", admin.email);
 }
 
 main()
   .catch((e) => {
     console.error(e);
-=======
-  console.log("✅ Admin créé :", user.email);
-}
-
-main()
-  .catch((error) => {
-    console.error(error);
->>>>>>> 7824227 (Fix login admin)
     process.exit(1);
   })
   .finally(async () => {
